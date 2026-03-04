815d961d4f21a94c132a7fa79121ec8a648a34a1
Lucien Deforge <125370560+Mangorix2@users.noreply.github.com>
2026-03-04 10:42:16 +0100

    feat: implement MultiHeadAttention

--- src/model.py
+++ src/model.py
@@ -2,7 +2,10 @@ import torch
 import torch.nn as nn
 from config import *
 
-class AttentionHead(nn.Module):
+class AttentionHead(nn.Module):    
+    # Ein einzelner AttentionHead (no shit)
+    # Er berechnet für jeden Token (Wort meistens) wie stark er auf andere achten soll -> Kontext
+
     def __init__(self):
         super().__init__()
 
@@ -26,4 +29,19 @@ class AttentionHead(nn.Module):
         attention_weights = torch.softmax(attention_scores, dim=-1)
         output = attention_weights @ values
         return output
-        
+
+class MultiHeadAttention(nn.Module):
+    # Hier laufen mehrere Attentionheads parallel um verschieden Kontext sachen zu machen
+    # Das Ergbnis wird dann zusammengetan und durchgemischt
+    
+    def __init__(self):
+        super().__init__()
+        self.heads = nn.ModuleList([
+            AttentionHead() for i in range(NUM_HEADS)
+        ])
+        self.output_projection = nn.Linear(EMBEDDING_DIM, EMBEDDING_DIM, bias=False)
+    
+    def forward(self, input_tensor):
+        concatenated = torch.cat([head(input_tensor) for head in self.heads], dim=-1)
+        return self.output_projection(concatenated)
+    
