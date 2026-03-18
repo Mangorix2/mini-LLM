9bafdd8157af4d0deba73c32e98a1dfd48178d0d
Lucien Deforge <125370560+Mangorix2@users.noreply.github.com>
2026-03-18 11:05:40 +0100

    feat: implement train with automatic model save and option for generating Text from the model

---
+++ src/generate.py
@@ -0,0 +1,35 @@
+import torch
+from model import SmallLM
+from data import build_tokenizer, load_text
+from config import *
+
+def generator(model, tokenizer, start_text, max_new_tokes=200):
+    model.eval() # bringt nichts im Moment aber später bei stärkeren LLMs schon
+
+    char_to_int, int_to_char = tokenizer
+    input_indices = [char_to_int[c] for c in start_text]
+    input_tensor = torch.tensor(input_indices).unsqueeze(0)
+
+    for i in range(max_new_tokes):
+        input_cropped = input_tensor[:, -CONTEXT_LEN:]
+
+        logits = model(input_cropped)
+        next_token_logits = logits[0,-1,:]
+
+        probs = torch.softmax(next_token_logits, dim=-1)
+        next_token = torch.multinomial(probs, num_samples=1)
+
+        input_tensor = torch.cat([input_tensor, next_token.unsqueeze(0)], dim=-1)  
+    
+    output_indices = input_tensor[0].tolist()
+    return ''.join([int_to_char[i] for i in output_indices])
+
+if __name__ == '__main__':
+    model = SmallLM()
+    model.load_state_dict(torch.load('checkpoint/epoch1_260318.pt'))
+    
+    text = load_text('data/input.txt')
+    tokenizer = build_tokenizer(text)
+    
+    output = generator(model, tokenizer, start_text="To be")
+    print(output)
--- src/train.py
+++ src/train.py
@@ -4,6 +4,73 @@ from torch.utils.data import DataLoader
 from model import SmallLM
 from data import TextDataset
 from config import *
+import time
+import glob
+import os
 
-def train():
-    ...
+def save_checkpoint(model, step, epoch=False):
+    checkpoints = sorted(
+        glob.glob("checkpoint/model_step*.pt"),
+        key=lambda x: int(x.split('step')[1].split('.')[0])
+    )
+    while len(checkpoints) > 4:
+        os.remove(checkpoints[0])
+        checkpoints.pop(0)
+
+    if(epoch):
+        torch.save(model.state_dict(), f"epochs/epoch{step}.pt")
+        return
+    
+    torch.save(model.state_dict(), f"checkpoint/model_step{step}.pt")
+
+    
+
+def train(resume_from=None, start_step=0):
+
+    global_step = start_step
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print(f"Training auf: {device}")
+
+    dataset = TextDataset('data/input.txt')
+    dataLoader = DataLoader(dataset, batch_size=32, shuffle=True)
+
+    model = SmallLM().to(device)
+    if resume_from:
+        model.load_state_dict(torch.load(resume_from))
+        print(f"Model geladen: {resume_from}")
+    
+    optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
+    loss_fn = nn.CrossEntropyLoss()
+
+    NUM_EPOCHS = 1
+
+    for epoch in range(NUM_EPOCHS):
+        epoch_start = time.time()
+
+        for step, (input_batch, target_batch) in enumerate(dataLoader):
+            global_step += 1
+
+            input_batch = input_batch.to(device)
+            target_batch = target_batch.to(device)
+
+            logits = model(input_batch)
+            loss = loss_fn(logits.view(-1, VOCAB_SIZE), target_batch.view(-1))
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step() 
+
+            if global_step % 100 == 0:
+                print(f"Epoch {epoch + 1}, Step {global_step}, Loss: {loss.item():.4f}")
+
+            if global_step % 500 == 0:
+                save_checkpoint(model, global_step)
+
+        epoch_time = time.time() - epoch_start
+        print(f"Epoch {epoch + 1 } fertig – Zeit: {epoch_time:.1f}s")
+        save_checkpoint(model, epoch + 1, epoch=True)
+
+
+if __name__ == '__main__':
+    train(resume_from='checkpoint/model_step12000.pt', start_step=12000)
