4d35e13c00102fb2bef1703cc55b2bf8c711df01
Lucien Deforge <125370560+Mangorix2@users.noreply.github.com>
2026-06-03 10:15:04 +0200

    feat: add interactive gen to see and choose next token in command line

--- src/generate.py
+++ src/generate.py
@@ -23,11 +23,39 @@ def generator(model, tokenizer, start_text, max_new_tokes=200):
     output_indices = input_tensor[0].tolist()
     return tokenizer.decode(output_indices)
 
+
+def interaktive_generator(model, tokenizer, start_txt, top_n=5):
+    model.eval()
+
+    input_indices = tokenizer.encode(start_txt)
+    input_tensor = torch.tensor(input_indices).unsqueeze(0)
+
+    while True:
+        input_cropped = input_tensor[:, -CONTEXT_LEN:]
+        logits = model(input_cropped)
+        probs = torch.softmax(logits[0,-1,:], dim=-1)
+
+        values, indices = torch.topk(probs, 5)
+
+        current_text = tokenizer.decode(input_cropped[0].tolist())
+        print(f"\nText: {current_text}")
+        for i, (val, idx) in enumerate(zip(values, indices)):
+            wort = tokenizer.decode([idx])
+            print(f"  {i}: {wort!r}  ({val:.2%})")
+
+        choice = input("Wahl (q=quit): ")
+        if choice == 'q':
+            break
+        
+        chosen_token = indices[int(choice)]
+        input_tensor = torch.cat([input_tensor, chosen_token.unsqueeze(0)], dim=-1)
+
+
 if __name__ == '__main__':
     model = SmaLLM()
     model.load_state_dict(torch.load('epochs/epoch4.pt'))
     
     tokenizer = build_tokenizer()
     
-    output = generator(model, tokenizer, start_text="To Be")
+    output = interaktive_generator(model, tokenizer, start_text="To Be")
     print(output)
